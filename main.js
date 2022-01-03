949f95c19c665980a0ebd21cfbc8c42abd150f31
Soumyadip005 <soumyaintplace@gmail.com>
2022-01-03 20:27:48 +0530

    mowz

---
+++ main.js
@@ -0,0 +1,51 @@
+window.addEventListener("DOMContentLoaded", function (){
+    var form = document.getElementById("my-form");
+    var status = document.getElementById("status");
+
+    function success() {
+        form.reset();
+        button.style = "display: none";
+        status.innerHTML = "Thanks!"
+    }
+
+    function error() {
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        this.insertAdjacentText(form.method, form.action, data, success, error);
+    });
+});
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("mBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
